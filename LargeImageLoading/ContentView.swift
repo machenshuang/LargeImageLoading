4c7a92effe18ae63d9885ec12eaf8e0a3d82b720
Sean <chenshuangma@foxmail.com>
2020-12-11 14:07:38 +0800

    feat：初始化

---
+++ LargeImageLoading/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LargeImageLoading
+//
+//  Created by 马陈爽 on 2020/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
